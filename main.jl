e2e2ebfabda6863f6578981ac932b506a1449a60
Benno Weber <bennopaul.weber@gmail.com>
2025-12-21 21:43:33 +0100

    wrong side...

--- main.jl
+++ main.jl
@@ -12,7 +12,7 @@ positions1_ana, positions2_ana = analyse_video(path)
 dt = 0.0000005
 time = 2
 
-pixelsperm = 2000
+pixelsperm = 1500       #totally invented value
 
 first_pos1_ana = positions1_ana[1]
 first_pos2_ana = positions2_ana[2]
@@ -21,13 +21,13 @@ anchor = position(0,0)
 point1 = position(first_pos1_ana[1]/pixelsperm,first_pos1_ana[2]/pixelsperm)
 point2 = position(first_pos2_ana[1]/pixelsperm,first_pos2_ana[2]/pixelsperm)
 
-m1 = 3
-m2 = 2
+m1 = 2
+m2 = 4
 
 w1_init = 0
 w2_init = 0
 
-method = "euler"
+method = "rk4"
 
 #launch simulation-----
 positions1_sim, positions2_sim = simulate(anchor, point1, point2, w1_init, w2_init, m1, m2, dt, time,method)
